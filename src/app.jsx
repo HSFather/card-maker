6ff0ebf7d60f6d5c84212d945dc425faececa9a6
HSFather <deepkim012@gmail.com>
2022-06-20 18:00:57 +0900

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./components/login/login";
+import styles from './app.module.css';
+
+
+function App({ authService }) {
+  return (
+    <div class={styles.app}>
+      <Login authService={authService} />;
+    </div>
+  );
+}
+
+export default App;
