481be091bafb7ea89e0734992f32b8ea2b201eda
HSFather <deepkim012@gmail.com>
2022-06-23 17:56:03 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: "1",
       name: "jinhan",
       company: "Samsung",
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: "jinhan",
       fileURL: null,
     },
-    {
+    2: {
       id: "2",
       name: "jinhan2",
       company: "Samsung",
@@ -30,7 +30,7 @@ const Maker = ({ authService }) => {
       fileName: "jinhan",
       fileURL: "",
     },
-    {
+    3: {
       id: "3",
       name: "jinhan3",
       company: "Samsung",
@@ -41,7 +41,8 @@ const Maker = ({ authService }) => {
       fileName: "jinhan",
       fileURL: null,
     },
-  ]);
+  });
+
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -55,15 +56,30 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
