12ca5133e1745ba682e08c21a64cd791c57da359
HSFather <deepkim012@gmail.com>
2022-06-23 14:22:12 +0900

    add card and edit form component

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./app.module.css";
 import Login from "./components/login/login";
-import styles from './app.module.css';
-
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   return (
-    <div class={styles.app}>
-      <Login authService={authService} />;
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login authService={authService} />}></Route>
+          <Route
+            path="/maker"
+            element={<Maker authService={authService} />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Editor from "../editor/editor";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import Preview from "../preview/preview";
+import styles from "./maker.module.css";
+
+const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "jinhan",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "jinhan@gmail.com",
+      message: "go for it",
+      fileName: "jinhan",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "jinhan2",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "jinhan@gmail.com",
+      message: "go for it",
+      fileName: "jinhan",
+      fileURL: "",
+    },
+    {
+      id: "3",
+      name: "jinhan3",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "jinhan@gmail.com",
+      message: "go for it",
+      fileName: "jinhan",
+      fileURL: null,
+    },
+  ]);
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor cards={cards} />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
